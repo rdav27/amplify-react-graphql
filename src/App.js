3cbc33c2c29acb0cc340d5c42d67bd406a07d563
rdav27 <137647196+rdav27@users.noreply.github.com>
2023-06-24 21:59:37 -0700

    charges for v2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -23,3 +23,23 @@ function App() {
 }
 
 export default App;
+*/
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
